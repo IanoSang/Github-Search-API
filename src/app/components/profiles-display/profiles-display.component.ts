efa2367e74885b9b521df552d5c0df3ed99ad48a
IanoSang <ianosang18@gmail.com>
2022-04-16 23:05:11 +0300

    feat: Services- creates a data service and imports imports into all components

--- src/app/components/profiles-display/profiles-display.component.ts
+++ src/app/components/profiles-display/profiles-display.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from "../../services/data.service";
 
 @Component({
   selector: 'app-profiles-display',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilesDisplayComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataService:DataService) { }
 
   ngOnInit(): void {
   }
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+
+  constructor( private http: HttpClient) {
+    console.log("")
+  }
+}
