fd5211f3b462977c1eebd0ae37c602954ad1113f
IanoSang <ianosang18@gmail.com>
2022-04-17 11:45:53 +0300

    feat: service- adds a template wher my profile will be displayed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms'
-
+import {HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfilesDisplayComponent } from './components/profiles-display/profiles-display.component';
 import { ReposDisplayComponent } from './components/repos-display/repos-display.component';
 
+// @ts-ignore
+// @ts-ignore
+// @ts-ignore
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,9 +19,11 @@ import { ReposDisplayComponent } from './components/repos-display/repos-display.
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
