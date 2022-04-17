53e1fbbf032fb3e92c463cb62633bb134fea320e
IanoSang <ianosang18@gmail.com>
2022-04-18 00:27:47 +0300

    feat: profile components- adds bio details and changes the navbar to navbar component

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -7,7 +7,7 @@ import { environment } from 'src/environments/environment';
 })
 export class DataService {
   public username:string= "IanoSang";
-  // public repoName!: string;
+  public userRepos!: string;
   public clientId: string = "69a77d3a9dcb6f2f7356";
   public clientSecret: string = "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";
   // public apikey:string='be05ad82ef1dec303061648a172836e81613bc78';
