9a95065b7bb8085af9e9bdb95ba9d8d7f7903ef7
IanoSang <ianosang18@gmail.com>
2022-04-19 10:24:02 +0300

    Auto-generated commit [ci skip]

--- src/app/button-highlight.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ButtonHighlightDirective } from './button-highlight.directive';
-
-describe('ButtonHighlightDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ButtonHighlightDirective();
-    expect(directive).toBeTruthy();
-  });
-});
